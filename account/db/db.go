1a6fc519d9509f5b4f74878156bb5feac4be876a
Somers B Matthews <somersbmatthews@gmail.com>
2021-01-12 03:58:26 -0600

    added some db.go code

--- account/db/db.go
+++ account/db/db.go
@@ -1,1 +1,30 @@
 package db
+
+import (
+	"fmt"
+
+	"somersbmatthews/instacook-micro/account/handler"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const database = "instacook.db"
+
+func init() {
+	db, err := gorm.Open(postgres.Open(database), &gorm.Config{})
+	if err != nil {
+		errMsg := fmt.Sprintf("failed to connect to database %s: %v", database, err)
+		panic(errMsg)
+	}
+
+	db.AutoMigrate(&handler.Account{})
+
+}
+
+func Create(acc &handler.Account) err {
+	
+	db.Create(acc)
+
+
+}
--- account/go.mod
+++ account/go.mod
@@ -11,7 +11,7 @@ require (
 	golang.org/x/text v0.3.5 // indirect
 	golang.org/x/tools v0.0.0-20210108195828-e2f9c7f1fc8e // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gorm.io/driver/postgres v1.0.6 // indirect
+	gorm.io/driver/postgres v1.0.6
 	gorm.io/gorm v1.20.11
 )
 
